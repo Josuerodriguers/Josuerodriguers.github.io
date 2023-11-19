c4ee4294c9804d9caf5a0d0caa404eb6fe49666c
Josue Azevedo <josuerodriguers03@gmail.com>
2023-11-19 06:49:55 -0300

    ajusta style center

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,7 +4,7 @@ export default [
     projectName: 'Password Manager',
     projectDescription: 'Projeto foi desenvolvido para ser um gerenciador de senhas, com a possibilidade de adicionar e remover senhas,',
     projectImage: 'password-manager.png',
-    projectUrl: 'https://github.com/Josuerodriguers/Gerenciador-Password/',
+    projectUrl: 'https://gerenciador-senhas.vercel.app',
     tags: ['React', 'TypeScript', 'React Hooks' , 'LocalStorage', 'Flexbox', 'CSS']
   },
 ]
